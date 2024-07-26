3813b9d6d0f9c2443b816e3905689cedb9da6774
Krapotur <dayfive2020@gmail.com>
2024-07-26 17:11:52 +0300

    Add form for login-page

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {NgIf, NgOptimizedImage} from "@angular/common";
+import {MatIconModule} from "@angular/material/icon";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatTooltipModule,
+    ReactiveFormsModule,
+    NgIf,
+    MatIconModule,
+    RouterLink,
+    NgOptimizedImage
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit, OnDestroy {
+  form: FormGroup
 
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      login: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  ngOnDestroy() {
+
+  }
+
+  onSubmit() {
+
+  }
 }
