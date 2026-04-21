0a2d77ff19d86d9f6db53f547212cfc55f5adf5b
Krapotur <dayfive2220@gmail.com>
2026-04-21 16:19:40 +0300

    Add alert dialog for logout

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,46 +1,51 @@
-import {Routes} from '@angular/router';
-import {AuthLayoutComponent} from "./shared/layouts/auth-layout/auth-layout.component";
-import {LoginPageComponent} from "./login-page/login-page.component";
-import {SiteLayoutComponent} from "./shared/layouts/site-layout/site-layout.component";
-import {isAuthGuard} from "./shared/classes/auth.guard";
-import {DashboardPageComponent} from "./admin-pages/dashboard-page/dashboard-page.component";
-import {OverviewPageComponent} from "./restaurateur-pages/overview-page/overview-page.component";
-import {UsersPageComponent} from "./admin-pages/users-page/users-page.component";
-import {RestaurantsPageComponent} from "./admin-pages/restaurants-page/restaurants-page.component";
-import {RestaurantFormComponent} from "./form-pages/restaurant-form/restaurant-form.component";
-import {UserFormComponent} from "./form-pages/user-form/user-form.component";
-import {CategoryFormComponent} from "./form-pages/category-form/category-form.component";
-import {KitchenFormComponent} from "./form-pages/kitchen-form/kitchen-form.component";
-import {PositionFormComponent} from "./form-pages/position-form/position-form.component";
-import {AssortmentComponent} from "./admin-pages/assortment/assortment.component";
-import {PositionsPageComponent} from "./admin-pages/positions-page/positions-page.component";
+import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { isAuthGuard } from './shared/classes/auth.guard';
+import { DashboardPageComponent } from './admin-pages/dashboard-page/dashboard-page.component';
+import { OverviewPageComponent } from './restaurateur-pages/overview-page/overview-page.component';
+import { UsersPageComponent } from './admin-pages/users-page/users-page.component';
+import { RestaurantsPageComponent } from './admin-pages/restaurants-page/restaurants-page.component';
+import { RestaurantFormComponent } from './form-pages/restaurant-form/restaurant-form.component';
+import { UserFormComponent } from './form-pages/user-form/user-form.component';
+import { CategoryFormComponent } from './form-pages/category-form/category-form.component';
+import { KitchenFormComponent } from './form-pages/kitchen-form/kitchen-form.component';
+import { PositionFormComponent } from './form-pages/position-form/position-form.component';
+import { AssortmentComponent } from './admin-pages/assortment/assortment.component';
+import { PositionsPageComponent } from './admin-pages/positions-page/positions-page.component';
 
 export const routes: Routes = [
   {
-    path: '', component: AuthLayoutComponent, children: [
-      {path: '', redirectTo: '/login', pathMatch: 'full'},
-      {path: 'login', component: LoginPageComponent}
-    ]
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: '', redirectTo: '/login', pathMatch: 'full' },
+      { path: 'login', component: LoginPageComponent },
+    ],
   },
   {
-    path: 'st', component: SiteLayoutComponent, children: [
-      {path: '', redirectTo: '/st/users', pathMatch: 'full'},
-      {path: 'dashboard', component: DashboardPageComponent},
-      {path: 'overview', component: OverviewPageComponent},
-      {path: 'users', component: UsersPageComponent},
-      {path: 'restaurants', component: RestaurantsPageComponent},
-      {path: 'positions', component: PositionsPageComponent},
-      {path: 'assortment', component: AssortmentComponent},
-      {path: 'form-user', component: UserFormComponent},
-      {path: 'form-user/:id', component: UserFormComponent},
-      {path: 'form-restaurant', component: RestaurantFormComponent},
-      {path: 'form-restaurant/:id', component: RestaurantFormComponent},
-      {path: 'form-kitchen', component: KitchenFormComponent},
-      {path: 'form-kitchen/:id', component: KitchenFormComponent},
-      {path: 'form-category', component: CategoryFormComponent},
-      {path: 'form-category/:id', component: CategoryFormComponent},
-      {path: 'form-position', component: PositionFormComponent},
-      {path: 'form-position/:id', component: PositionFormComponent},
-    ]
-  }
+    path: 'st',
+    component: SiteLayoutComponent,
+    canActivate: [isAuthGuard],
+    children: [
+      { path: '', redirectTo: '/st/users', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardPageComponent },
+      { path: 'overview', component: OverviewPageComponent },
+      { path: 'users', component: UsersPageComponent },
+      { path: 'restaurants', component: RestaurantsPageComponent },
+      { path: 'positions', component: PositionsPageComponent },
+      { path: 'assortment', component: AssortmentComponent },
+      { path: 'form-user', component: UserFormComponent },
+      { path: 'form-user/:id', component: UserFormComponent },
+      { path: 'form-restaurant', component: RestaurantFormComponent },
+      { path: 'form-restaurant/:id', component: RestaurantFormComponent },
+      { path: 'form-kitchen', component: KitchenFormComponent },
+      { path: 'form-kitchen/:id', component: KitchenFormComponent },
+      { path: 'form-category', component: CategoryFormComponent },
+      { path: 'form-category/:id', component: CategoryFormComponent },
+      { path: 'form-position', component: PositionFormComponent },
+      { path: 'form-position/:id', component: PositionFormComponent },
+    ],
+  },
 ];
--- client/src/app/shared/layouts/site-layout/site-layout.component.ts
+++ client/src/app/shared/layouts/site-layout/site-layout.component.ts
@@ -1,79 +1,84 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
-import { NgIf } from "@angular/common";
-import { AuthService } from "../../services/auth.service";
-import { MaterialService } from "../../classes/material.service";
-import { GroupService } from "../../services/group.service";
-import { Subscription } from "rxjs";
-import { RestaurantService } from "../../services/restaurant.service";
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
+import { NgIf } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { MaterialService } from '../../classes/material.service';
+import { GroupService } from '../../services/group.service';
+import { Subscription } from 'rxjs';
+import { RestaurantService } from '../../services/restaurant.service';
 import { SharedService } from '../../services/shared.service';
 
 @Component({
   selector: 'app-site-layout',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    RouterLink,
-    NgIf,
-    RouterLinkActive,
-  ],
+  imports: [RouterOutlet, RouterLink, NgIf, RouterLinkActive],
   templateUrl: './site-layout.component.html',
-  styleUrl: './site-layout.component.scss'
+  styleUrl: './site-layout.component.scss',
 })
 export class SiteLayoutComponent implements OnInit, OnDestroy {
-  private authService = inject(AuthService)
-  private sharedService = inject(SharedService)
-  private groupService = inject(GroupService)
-  private restaurantService = inject(RestaurantService)
-  private router = inject(Router)
+  private authService = inject(AuthService);
+  private sharedService = inject(SharedService);
+  private groupService = inject(GroupService);
+  private restaurantService = inject(RestaurantService);
+  private router = inject(Router);
 
-  isAdmin: boolean
-  gSub: Subscription
-  rSub: Subscription
-  userName = ''
-  restaurantTitle = ''
+  isAdmin: boolean;
+  isLogout = false;
+  gSub: Subscription;
+  rSub: Subscription;
+  userName = '';
+  restaurantTitle = '';
 
   ngOnInit() {
-    this.getGroupById()
-    this.getRestaurantByUser()
+    this.getGroupById();
+    this.getRestaurantByUser();
   }
 
   ngOnDestroy() {
-    if (this.gSub) this.gSub.unsubscribe()
-    if (this.rSub) this.rSub.unsubscribe()
+    if (this.gSub) this.gSub.unsubscribe();
+    if (this.rSub) this.rSub.unsubscribe();
   }
 
   getGroupById() {
-    let profile = JSON.parse(localStorage.getItem('profile'))
+    let profile = JSON.parse(localStorage.getItem('profile'));
     this.gSub = this.groupService.getGroupByID(profile.group).subscribe({
-      next: group => { 
-        this.sharedService.updateDataGroup(group.alias)
-        this.isAdmin = group.alias === 'administrator'; 
+      next: (group) => {
+        this.sharedService.updateDataGroup(group.alias);
+        this.isAdmin = group.alias === 'administrator';
       },
-      error: error => MaterialService.toast(error.error.error)
-    })
+      error: (error) => MaterialService.toast(error.error.error),
+    });
   }
 
   getRestaurantByUser() {
-    let profile
+    let profile;
     if (localStorage.getItem('profile')) {
-      profile = JSON.parse(localStorage.getItem('profile'))
+      profile = JSON.parse(localStorage.getItem('profile'));
     }
-    this.getRestaurantById(profile['rest'])
-    this.userName = profile['userName']
+    this.getRestaurantById(profile['rest']);
+    this.userName = profile['userName'];
   }
 
   getRestaurantById(id: string) {
     if (id) {
       this.rSub = this.restaurantService.getRestaurantByID(id).subscribe({
-        next: rest => this.restaurantTitle = rest.title,
-        error: e => MaterialService.toast(e.error.message)
-      })
+        next: (rest) => (this.restaurantTitle = rest.title),
+        error: (e) => MaterialService.toast(e.error.message),
+      });
     }
   }
 
   logout() {
-    this.authService.logout()
-    void this.router.navigate(['/login'])
+    this.isLogout = !this.isLogout;
+  }
+
+  submit() {
+    this.authService.logout();
+    void this.router.navigate(['/login']);
   }
 }
